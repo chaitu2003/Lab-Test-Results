b67ba94551287235feb96f14c4521e227798289a
Chaitanya Annam <132034829+chaitu2003@users.noreply.github.com>
2025-05-29 00:08:05 +0530

    Update lab_test_results.js

--- assets/js/lab_test_results.js
+++ assets/js/lab_test_results.js
@@ -8,20 +8,20 @@ function cal(event){
             let msg = [];
 
             if(glucose < 70) {
-                msg.push('Glucose is LOW. (Possible hypoglycemia)');
+                msg.push('Glucose is LOW. <br> (Possible hypoglycemia)');
             }
             else if (glucose > 99) {
-                msg.push('Glucose is HIGH. (May indicate diabetes risk)');
+                msg.push('Glucose is HIGH. <br> (May indicate diabetes risk)');
             } 
             else {
                 msg.push('Glucose is NORMAL.');
             }
 
             if (hemoglobin < 13.8) {
-                msg.push(`<br>Hemoglobin is LOW. (Possible anemia)`);
+                msg.push(`<br>Hemoglobin is LOW. <br> (Possible anemia)`);
             } 
             else if (hemoglobin > 17.2) {
-                msg.push(`<br>Hemoglobin is HIGH. (Possible dehydration or other issues)`);
+                msg.push(`<br>Hemoglobin is HIGH. <br> (Possible dehydration or other issues)`);
             } 
             else {
                 msg.push(`<br>Hemoglobin is NORMAL.`);
